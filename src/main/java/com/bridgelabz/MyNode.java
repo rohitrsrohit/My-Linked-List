bb75e514e096a255ac4a49caa709c213115f22ed
Rohit Patro <rohitpatro6@gmail.com>
2021-08-10 11:55:55 +0530

    ADDED:Creation of a simple LinkedList Added

--- src/main/java/com/bridgelabz/MyNode.java
+++ src/main/java/com/bridgelabz/MyNode.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
-public class MyNode {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Linked List");
+public class MyNode<T> {
+    private T key;
+    private MyNode<T> next;
+
+    public MyNode(T key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode<T> getNext() {
+        return next;
+    }
+    public void setNext(MyNode<T> next) {
+        this.next = next;
     }
 }
